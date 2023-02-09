cc3025271af541c2eabd3c806d26146bae6497e2
tjoeun <tjoeun@DESKTOP-PJLQ947>
2023-02-09 12:51:42 +0900

    공부

---
+++ src/com/greedy/section02/uses/Application01.java
@@ -0,0 +1,10 @@
+package com.greedy.section02.uses;
+
+public class Application01 {
+
+	public static void main(String[] args) {
+
+		new B_OtherClassTest();
+	}
+
+}
---
+++ src/com/greedy/section03/change/Application01.java
@@ -0,0 +1,11 @@
+package com.greedy.section03.change;
+
+public class Application01 {
+
+	public static void main(String[] args) {
+		
+		new MainFrame();
+	}
+
+}
+ 
